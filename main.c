dba22f074180bc8ac43cff8e302d26eae0f16670
Bennett Rennier <bennett@brennier.com>
2025-07-11 19:15:08 +0900

    Read rom into memory and try the first instruction

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include "include/raylib.h"
 #include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 #define SCREENWIDTH 166
 #define SCREENHEIGHT 144
@@ -13,18 +16,49 @@ union {
                 uint8_t af, bc, de, hl;
         };
         uint8_t raw_bytes[12];
-} reg;
+} gb_register;
+
+// The GB has 64kb of mapped memory
+uint8_t gb_memory[64 * 1024];
+
+void load_rom(char* rom_name, size_t rom_bytes) {
+    FILE *rom_file = fopen(rom_name, "rb"); // binary read mode
+    if (rom_file == NULL) {
+        fprintf(stderr, "Error opening the rom file. Exiting...\n");
+        exit(EXIT_FAILURE);
+    }
+
+    size_t bytes_read = fread(gb_memory, 1, rom_bytes, rom_file);
+    if (bytes_read != rom_bytes) {
+        fprintf(stderr, "Error reading from the rom file. Exiting...\n");
+        exit(EXIT_FAILURE);
+    }
+    fclose(rom_file);
+}
 
 int main() {
-        InitWindow(SCREENWIDTH, SCREENHEIGHT, "GameBoy Emulator");
+        load_rom("test.gb", 32 * 1024);
+        gb_register.pc = 0x100;
 
-        while (WindowShouldClose() != true) {
-                BeginDrawing();
-                ClearBackground(BLACK);
-                DrawFPS(10, 10);
-                EndDrawing();
+        while (true) {
+            uint8_t op_byte = gb_memory[gb_register.pc];
+            switch (op_byte) {
+
+            default:
+                fprintf(stderr, "Op Code not implemented: 0x%02X\n", op_byte);
+                exit(EXIT_FAILURE);
+                break;
+            }
         }
 
-        CloseWindow();
+        /* InitWindow(SCREENWIDTH, SCREENHEIGHT, "GameBoy Emulator"); */
+        /* while (WindowShouldClose() != true) { */
+        /*         BeginDrawing(); */
+        /*         ClearBackground(BLACK); */
+        /*         DrawFPS(10, 10); */
+        /*         EndDrawing(); */
+        /* } */
+        /* CloseWindow(); */
+
         return 0;
 }
