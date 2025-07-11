c8f228e2cce81ca87d9ffeea98ecd4e729dd1408
Bennett Rennier <bennett@brennier.com>
2025-07-11 20:37:39 +0900

    Fix indentation

--- main.c
+++ main.c
@@ -8,57 +8,57 @@
 #define SCREENHEIGHT 144
 
 union {
-        struct {
-                uint8_t f, a, c, b, e, d, l, h;
-                uint16_t sp, pc;
-        };
-        struct {
-                uint8_t af, bc, de, hl;
-        };
-        uint8_t raw_bytes[12];
+	struct {
+		uint8_t f, a, c, b, e, d, l, h;
+		uint16_t sp, pc;
+	};
+	struct {
+		uint8_t af, bc, de, hl;
+	};
+	uint8_t raw_bytes[12];
 } gb_register;
 
 // The GB has 64kb of mapped memory
 uint8_t gb_memory[64 * 1024];
 
 void load_rom(char* rom_name, size_t rom_bytes) {
-    FILE *rom_file = fopen(rom_name, "rb"); // binary read mode
-    if (rom_file == NULL) {
-        fprintf(stderr, "Error opening the rom file. Exiting...\n");
-        exit(EXIT_FAILURE);
-    }
+	FILE *rom_file = fopen(rom_name, "rb"); // binary read mode
+	if (rom_file == NULL) {
+		fprintf(stderr, "Error opening the rom file. Exiting...\n");
+		exit(EXIT_FAILURE);
+	}
 
-    size_t bytes_read = fread(gb_memory, 1, rom_bytes, rom_file);
-    if (bytes_read != rom_bytes) {
-        fprintf(stderr, "Error reading from the rom file. Exiting...\n");
-        exit(EXIT_FAILURE);
-    }
-    fclose(rom_file);
+	size_t bytes_read = fread(gb_memory, 1, rom_bytes, rom_file);
+	if (bytes_read != rom_bytes) {
+		fprintf(stderr, "Error reading from the rom file. Exiting...\n");
+		exit(EXIT_FAILURE);
+	}
+	fclose(rom_file);
 }
 
 int main() {
-        load_rom("test.gb", 32 * 1024);
-        gb_register.pc = 0x100;
+	load_rom("test.gb", 32 * 1024);
+	gb_register.pc = 0x100;
 
-        while (true) {
-            uint8_t op_byte = gb_memory[gb_register.pc];
-            switch (op_byte) {
+	while (true) {
+		uint8_t op_byte = gb_memory[gb_register.pc];
+		switch (op_byte) {
 
-            default:
-                fprintf(stderr, "Op Code not implemented: 0x%02X\n", op_byte);
-                exit(EXIT_FAILURE);
-                break;
-            }
-        }
+		default:
+			fprintf(stderr, "Op Code not implemented: 0x%02X\n", op_byte);
+			exit(EXIT_FAILURE);
+			break;
+		}
+	}
 
-        /* InitWindow(SCREENWIDTH, SCREENHEIGHT, "GameBoy Emulator"); */
-        /* while (WindowShouldClose() != true) { */
-        /*         BeginDrawing(); */
-        /*         ClearBackground(BLACK); */
-        /*         DrawFPS(10, 10); */
-        /*         EndDrawing(); */
-        /* } */
-        /* CloseWindow(); */
+	/* InitWindow(SCREENWIDTH, SCREENHEIGHT, "GameBoy Emulator"); */
+	/* while (WindowShouldClose() != true) { */
+	/*	   BeginDrawing(); */
+	/*	   ClearBackground(BLACK); */
+	/*	   DrawFPS(10, 10); */
+	/*	   EndDrawing(); */
+	/* } */
+	/* CloseWindow(); */
 
-        return 0;
+	return 0;
 }
