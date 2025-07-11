d9dbd745ddf338ac69ab9b50ace1b84259fc1691
Bennett Uezu <barennier@gmail.com>
2025-07-11 16:31:18 +0900

    Define the CPU registers

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include "include/raylib.h"
+#include <stdint.h>
 
 #define SCREENWIDTH 166
 #define SCREENHEIGHT 144
+
+union {
+        struct {
+                uint8_t f, a, c, b, e, d, l, h;
+                uint16_t sp, pc;
+        };
+        struct {
+                uint8_t af, bc, de, hl;
+        };
+        uint8_t raw_bytes[12];
+} reg;
 
 int main() {
         InitWindow(SCREENWIDTH, SCREENHEIGHT, "GameBoy Emulator");
