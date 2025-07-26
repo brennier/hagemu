75653d7f2045b76107bc2d3d7fa338fe8d42ad0f
Bennett Rennier <bennett@brennier.com>
2025-07-26 13:57:35 +0900

    Display tile map 0

--- src/main.c
+++ src/main.c
@@ -16,8 +16,8 @@
 #define SCALE_FACTOR 5
 #define SCREENWIDTH 166 * SCALE_FACTOR
 #define SCREENHEIGHT 144 * SCALE_FACTOR
-#define SPEED_FACTOR 1
-#define CLOCK_SPEED 4194304 * SPEED_FACTOR
+#define SPEED_FACTOR 20
+#define CLOCK_SPEED 4194304L * SPEED_FACTOR
 
 void debug_blargg_check_serial() {
 	// Check for the word "Passed" and exit successfuly if detected
@@ -91,9 +91,6 @@ void load_tile_data_block(Texture2D *tile_data_block, uint16_t address_start) {
 int main(int argc, char *argv[]) {
 	cpu_reset();
 
-	// The gameboy doctor test suite requires that the LY register always returns 0x90
-	mmu_write(0xFF44, 0x90);
-
 	if (argc == 1) {
 		fprintf(stderr, "Error: No rom file specified\n");
 		exit(EXIT_FAILURE);
@@ -115,10 +112,11 @@ int main(int argc, char *argv[]) {
 	UnloadImage(tile_image);
 
 	while (WindowShouldClose() != true) {
-		int cycles_since_last_frame = 0;
+		long cycles_since_last_frame = 0;
 		float delta_time = GetFrameTime();
-		while (cycles_since_last_frame < delta_time * CLOCK_SPEED)
+		while (cycles_since_last_frame < delta_time * CLOCK_SPEED) {
 			cycles_since_last_frame += cpu_do_next_instruction();
+		}
 
 		load_tile_data_block(&tile_data_block_1, 0x8000);
 		load_tile_data_block(&tile_data_block_2, 0x8800);
@@ -126,12 +124,22 @@ int main(int argc, char *argv[]) {
 
 		BeginDrawing();
 		ClearBackground(BLACK);
-		for (int i = 0; i < 8; i++)
-			DrawTexturePro(tile_data_block_1, (Rectangle){ .x = 0, .y = 128 * i, .width = 8, .height = 128 }, (Rectangle){ .x = 32 * i, .y = 0, .width = 32, .height = 512 }, (Vector2){ 0, 0 }, 0.0, WHITE);
-		for (int i = 0; i < 8; i++)
-			DrawTexturePro(tile_data_block_2, (Rectangle){ .x = 0, .y = 128 * i, .width = 8, .height = 128 }, (Rectangle){ .x = 32 * i + 256 + 32, .y = 0, .width = 32, .height = 512 }, (Vector2){ 0, 0 }, 0.0, WHITE);
-		for (int i = 0; i < 8; i++)
-			DrawTexturePro(tile_data_block_3, (Rectangle){ .x = 0, .y = 128 * i, .width = 8, .height = 128 }, (Rectangle){ .x = 32 * i + 512 + 64, .y = 0, .width = 32, .height = 512 }, (Vector2){ 0, 0 }, 0.0, WHITE);
+		/* for (int i = 0; i < 8; i++) */
+		/* DrawTexturePro(tile_data_block_1, (Rectangle){ .x = 0, .y = 128 * i, .width = 8, .height = 128 }, (Rectangle){ .x = 32 * i, .y = 0, .width = 32, .height = 512 }, (Vector2){ 0, 0 }, 0.0, WHITE); */
+		/* for (int i = 0; i < 8; i++) */
+		/* DrawTexturePro(tile_data_block_2, (Rectangle){ .x = 0, .y = 128 * i, .width = 8, .height = 128 }, (Rectangle){ .x = 32 * i + 256 + 32, .y = 0, .width = 32, .height = 512 }, (Vector2){ 0, 0 }, 0.0, WHITE); */
+		/* for (int i = 0; i < 8; i++) */
+		/* DrawTexturePro(tile_data_block_3, (Rectangle){ .x = 0, .y = 128 * i, .width = 8, .height = 128 }, (Rectangle){ .x = 32 * i + 512 + 64, .y = 0, .width = 32, .height = 512 }, (Vector2){ 0, 0 }, 0.0, WHITE); */
+
+		uint16_t tile_map_start = 0x9800;
+		for (int row = 0; row < 32; row++)
+			for (int col = 0; col < 32; col++) {
+				int tile_pos = mmu_read(tile_map_start + row * 32 + col);
+				if (tile_pos < 128)
+					DrawTexturePro(tile_data_block_1, (Rectangle){ .x = 0, .y = 8 * tile_pos, .width = 8, .height = 8 }, (Rectangle){ .x = col * 16, .y = row * 16, .width = 16, .height = 16}, (Vector2){ 0, 0}, 0.0, WHITE);
+				else
+					DrawTexturePro(tile_data_block_2, (Rectangle){ .x = 0, .y = 8 * (tile_pos - 128), .width = 8, .height = 8 }, (Rectangle){ .x = col * 8, .y = row * 8, .width = 8, .height = 8}, (Vector2){ 0, 0}, 0.0, WHITE);
+			}
 		DrawFPS(10, 10);
 		EndDrawing();
 	}
