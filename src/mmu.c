1751f430bf66b2c3fd2342998f2b8a5f9cd9b322
Bennett Rennier <bennett@brennier.com>
2025-08-20 20:18:55 +0900

    Route addresses 0xFF10 through 0xFF3F to the APU

--- src/mmu.c
+++ src/mmu.c
@@ -73,14 +73,6 @@ uint8_t mmu_read(uint16_t address) {
 	case INTERRUPT_FLAGS:
 		// bits 3 through 7 should always be 1
 		return gb_memory[INTERRUPT_FLAGS] | 0xE0;
-
-	case SOUND_NR10: case SOUND_NR11: case SOUND_NR12: case SOUND_NR13:
-	case SOUND_NR14: case SOUND_NR21: case SOUND_NR22: case SOUND_NR23:
-	case SOUND_NR24: case SOUND_NR30: case SOUND_NR31: case SOUND_NR32:
-	case SOUND_NR33: case SOUND_NR34: case SOUND_NR41: case SOUND_NR42:
-	case SOUND_NR43: case SOUND_NR44: case SOUND_NR50: case SOUND_NR51:
-	case SOUND_NR52:
-		return apu_audio_register_read(address);
 	}
 
 	switch (address & 0xF000) {
@@ -120,7 +112,13 @@ uint8_t mmu_read(uint16_t address) {
 		// Unusable memory
 		else if (address < 0xFEFF)
 			return 0xFF;
-		// TODO: IO Registers and High RAM
+		// IO registers
+		else if (address < 0xFF10)
+			return gb_memory[address];
+		// Send to APU
+		else if (address < 0xFF40)
+			return apu_audio_register_read(address);
+		// More IO registers and high ram
 		else
 			return gb_memory[address];
 	}
@@ -153,15 +151,6 @@ void mmu_write(uint16_t address, uint8_t value) {
 	case LCD_Y_COORDINATE:
 		printf("Illegal write to LCD Y Coordinate. Ignoring...\n");
 		return;
-
-	case SOUND_NR10: case SOUND_NR11: case SOUND_NR12: case SOUND_NR13:
-	case SOUND_NR14: case SOUND_NR21: case SOUND_NR22: case SOUND_NR23:
-	case SOUND_NR24: case SOUND_NR30: case SOUND_NR31: case SOUND_NR32:
-	case SOUND_NR33: case SOUND_NR34: case SOUND_NR41: case SOUND_NR42:
-	case SOUND_NR43: case SOUND_NR44: case SOUND_NR50: case SOUND_NR51:
-	case SOUND_NR52:
-	    apu_audio_register_write(address, value);
-	    return;
 	}
 
 	switch (address & 0xF000) {
@@ -223,25 +212,24 @@ void mmu_write(uint16_t address, uint8_t value) {
 
 	case 0xE000: case 0xF000:
 		// Echo RAM (about 8 KiB)
-		if (address < 0xFE00) {
+		if (address < 0xFE00)
 			gb_memory[address - 0x2000] = value;
-			return;
-		}
 		// Object Attribute Memory
-		else if (address < 0xFEA0) {
+		else if (address < 0xFEA0)
 			gb_memory[address] = value;
+		// Unusable forbidden memory
+		else if (address < 0xFEFF)
 			return;
-		}
-		// Unusable memory
-		else if (address < 0xFEFF) {
-			// fprintf(stderr, "Warning: Attempted write at the forbidden address '%02X'. Ignoring...\n", address);
-			return;
-		}
-		// TODO: IO Registers and High RAM
-		else {
+		// IO registers
+		else if (address < 0xFF10)
 			gb_memory[address] = value;
-			return;
-		}
+		// Send to APU
+		else if (address < 0xFF40)
+			apu_audio_register_write(address, value);
+		// More IO registers and high ram
+		else
+			gb_memory[address] = value;
+		return;
 	}
 
 	fprintf(stderr, "Error: Illegal memory access at location `0x%04X'", address);
