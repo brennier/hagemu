854721675e99cf26ddcb2ca7e65ee926ae603bec
Bennett Rennier <bennett@brennier.com>
2025-08-28 15:57:28 +0900

    Separate the draw background and draw window functions

--- src/ppu.c
+++ src/ppu.c
@@ -26,13 +26,16 @@ R5G5B5A1 convert_color(unsigned red, unsigned green, unsigned blue) {
 }
 
 R5G5B5A1 screen_buffer[144][160];
+
 bool is_background_nonzero[160];
 int current_line = 0;
 int current_window_line = 0;
 bool window_triggered = false;
 void ppu_draw_scanline();
 void ppu_draw_sprites();
-void ppu_draw_background(bool tile_map_mode, uint8_t bg_row, int bg_col, bool drawing_window);
+void ppu_draw_background();
+void ppu_draw_window(uint8_t bg_row, int bg_col);
+void apply_palette(uint16_t *array_2bbp, int array_length, uint16_t palette_location);
 
 enum PPUMode {
 	HBLANK,
@@ -106,26 +109,19 @@ void ppu_update(int current_cycle) {
 }
 
 bool ppu_frame_finished(int current_cycle) {
-	if (current_cycle > 70224)
-		return true;
-	else
-		return false;
+	return current_cycle > 70224;
 }
 
 void ppu_draw_scanline() {
 	if (mmu_get_bit(BG_ENABLE)) {
-		bool tile_map_mode = mmu_get_bit(BG_TILE_MAP_AREA);
-		uint8_t bg_row = current_line + mmu_read(BG_SCROLL_Y);
-		uint8_t bg_start_col = mmu_read(BG_SCROLL_X);
-		ppu_draw_background(tile_map_mode, bg_row, bg_start_col, false);
+		ppu_draw_background();
 
 		if (mmu_read(WIN_SCROLL_Y) == current_line)
 			window_triggered = true;
-		
+
 		int win_start_col = mmu_read(WIN_SCROLL_X) - 7;
 		if (window_triggered && mmu_get_bit(WINDOW_ENABLE) && win_start_col < 160) {
-			tile_map_mode = mmu_get_bit(WINDOW_TILE_MAP_AREA);
-			ppu_draw_background(tile_map_mode, current_window_line, win_start_col, true);
+			ppu_draw_window(current_window_line, win_start_col);
 			current_window_line++;
 		}
 	} else {
@@ -135,53 +131,82 @@ void ppu_draw_scanline() {
 
 	if (mmu_get_bit(OBJECTS_ENABLE))
 		ppu_draw_sprites();
+
 }
 
-void apply_palette(uint16_t *array_2bbp, int array_length, uint16_t palette_location) {
-	uint8_t palette_data = mmu_read(palette_location);
-	R5G5B5A1 palette_color0 = ppu_default_colors[palette_data & 0x03];
-	palette_data >>= 2;
-	R5G5B5A1 palette_color1 = ppu_default_colors[palette_data & 0x03];
-	palette_data >>= 2;
-	R5G5B5A1 palette_color2 = ppu_default_colors[palette_data & 0x03];
-	palette_data >>= 2;
-	R5G5B5A1 palette_color3 = ppu_default_colors[palette_data & 0x03];
-
-	for (int i = 0; i < array_length; i++) {
-		switch (array_2bbp[i]) {
-			
-			case 0: array_2bbp[i] = palette_color0; break;
-			case 1: array_2bbp[i] = palette_color1; break;
-			case 2: array_2bbp[i] = palette_color2; break;
-			case 3: array_2bbp[i] = palette_color3; break;
-		}
-	}
-}
-
-void ppu_draw_background(bool tile_map_mode, uint8_t bg_row, int bg_col, bool drawing_window) {
-	int background_row = bg_row;
-
+void ppu_draw_background() {
 	// Get tile indices
 	uint8_t tile_indices[32];
-	int tile_index_row = background_row / 8;
-	int tile_pixel_row = background_row % 8;
-	uint16_t tile_map_start;
-	if (tile_map_mode)
-		tile_map_start = 0x9C00;
-	else
-		tile_map_start = 0x9800;
+	uint8_t bg_row = current_line + mmu_read(BG_SCROLL_Y);
+	int tile_index_row = bg_row / 8;
+	int tile_pixel_row = bg_row % 8;
+	uint16_t tile_map_start = mmu_get_bit(BG_TILE_MAP_AREA) ? 0x9C00 : 0x9800;
 
 	for (int col = 0; col < 32; col++)
 		tile_indices[col] = mmu_read(tile_map_start + 32 * tile_index_row + col);
 
 	// Convert indices to raw tile data
 	uint8_t raw_tile_data[32 * 2];
-	uint16_t data_block_1;
+	uint16_t data_block_1 = mmu_get_bit(BG_TILE_DATA_AREA) ? 0x8000 : 0x9000;
 	uint16_t data_block_2 = 0x8800;
-	if (mmu_get_bit(BG_TILE_DATA_AREA))
-		data_block_1 = 0x8000;
-	else
-		data_block_1 = 0x9000;
+
+	for (int col = 0; col < 32; col++) {
+		uint16_t tile_start;
+		if (tile_indices[col] < 128)
+			tile_start = data_block_1 + 16 * tile_indices[col] + 2 * tile_pixel_row;
+		else
+			tile_start = data_block_2 + 16 * (tile_indices[col] - 128) + 2 * tile_pixel_row;
+		raw_tile_data[2 * col] = mmu_read(tile_start);
+		raw_tile_data[2 * col + 1] = mmu_read(tile_start + 1);
+	}
+
+	// Convert raw tile data into 2bpp format
+	uint16_t tile_data[256];
+	for (int i = 0; i < 64; i += 2) {
+		uint8_t byte1 = raw_tile_data[i];
+		uint8_t byte2 = raw_tile_data[i + 1];
+		for (int col = 0; col < 8; col++) {
+			bool bit1 = (byte1 & 0x80) >> 7;
+			bool bit2 = (byte2 & 0x80) >> 7;
+			tile_data[i * 4 + col] = (bit2 << 1) | bit1;
+			byte1 <<= 1;
+			byte2 <<= 1;
+		}
+	}
+
+	uint8_t bg_start_col = mmu_read(BG_SCROLL_X);
+	uint16_t cropped_tile_data[160];
+	for (int i = 0; i < 160; i++)
+		cropped_tile_data[i] = tile_data[((uint8_t)bg_start_col + i) % 256];
+
+	for (int i = 0; i < 160; i++)
+		if (cropped_tile_data[i] != 0)
+			is_background_nonzero[i] = true;
+		else
+			is_background_nonzero[i] = false;
+
+	// Convert 2bpp format to RGBA5551 format
+	apply_palette(cropped_tile_data, 160, BG_PALETTE);
+
+	// Copy scanline to screen_buffer
+	for (int i = 0; i < 160; i++)
+		screen_buffer[current_line][i] = cropped_tile_data[i];
+}
+
+void ppu_draw_window(uint8_t window_row, int col_start) {
+	// Get tile indices
+	uint8_t tile_indices[32];
+	int tile_index_row = window_row / 8;
+	int tile_pixel_row = window_row % 8;
+	uint16_t tile_map_start = mmu_get_bit(WINDOW_TILE_MAP_AREA) ? 0x9C00 : 0x9800;
+
+	for (int col = 0; col < 32; col++)
+		tile_indices[col] = mmu_read(tile_map_start + 32 * tile_index_row + col);
+
+	// Convert indices to raw tile data
+	uint8_t raw_tile_data[32 * 2];
+	uint16_t data_block_1 = mmu_get_bit(BG_TILE_DATA_AREA) ? 0x8000 : 0x9000;
+	uint16_t data_block_2 = 0x8800;
 
 	for (int col = 0; col < 32; col++) {
 		uint16_t tile_start;
@@ -208,40 +233,24 @@ void ppu_draw_background(bool tile_map_mode, uint8_t bg_row, int bg_col, bool dr
 	}
 
 	uint16_t cropped_tile_data[160];
-	if (drawing_window) {
-		for (int i = bg_col; i < 160; i++) {
-			if (i < 0)
-				continue;
-			cropped_tile_data[i] = tile_data[i - bg_col];
-			if (cropped_tile_data[i] != 0)
-				is_background_nonzero[i] = true;
-			else
-				is_background_nonzero[i] = false;
-		}
-		apply_palette(cropped_tile_data + bg_col, 160 - bg_col, BG_PALETTE);
-		for (int i = bg_col; i < 160; i++) {
-			if (i < 0)
-				continue;
-			screen_buffer[current_line][i] = cropped_tile_data[i];
-		}
-	} else {
-		for (int i = 0; i < 160; i++)
-			cropped_tile_data[i] = tile_data[((uint8_t)bg_col + i) % 256];
+	for (int i = col_start; i < 160; i++) {
+		if (i < 0)
+			continue;
+		cropped_tile_data[i] = tile_data[i - col_start];
+		if (cropped_tile_data[i] != 0)
+			is_background_nonzero[i] = true;
+		else
+			is_background_nonzero[i] = false;
+	}
 
-		for (int i = 0; i < 160; i++)
-			if (cropped_tile_data[i] != 0)
-				is_background_nonzero[i] = true;
-			else
-				is_background_nonzero[i] = false;
-
-		// Convert 2bpp format to RGBA5551 format
-		apply_palette(cropped_tile_data, 160, BG_PALETTE);
-
-		// Copy scanline to screen_buffer
-		for (int i = 0; i < 160; i++)
-			screen_buffer[current_line][i] = cropped_tile_data[i];
+	apply_palette(cropped_tile_data + col_start, 160 - col_start, BG_PALETTE);
+	for (int i = col_start; i < 160; i++) {
+		if (i < 0)
+			continue;
+		screen_buffer[current_line][i] = cropped_tile_data[i];
 	}
 }
+
 
 void ppu_draw_sprites() {
 	uint16_t oam_start = 0xFE00;
@@ -349,4 +358,25 @@ void ppu_draw_sprites() {
 
 R5G5B5A1* ppu_get_frame() {
 	return (R5G5B5A1*)screen_buffer;
+}
+
+void apply_palette(uint16_t *array_2bbp, int array_length, uint16_t palette_location) {
+	uint8_t palette_data = mmu_read(palette_location);
+	R5G5B5A1 palette_color0 = ppu_default_colors[palette_data & 0x03];
+	palette_data >>= 2;
+	R5G5B5A1 palette_color1 = ppu_default_colors[palette_data & 0x03];
+	palette_data >>= 2;
+	R5G5B5A1 palette_color2 = ppu_default_colors[palette_data & 0x03];
+	palette_data >>= 2;
+	R5G5B5A1 palette_color3 = ppu_default_colors[palette_data & 0x03];
+
+	for (int i = 0; i < array_length; i++) {
+		switch (array_2bbp[i]) {
+
+			case 0: array_2bbp[i] = palette_color0; break;
+			case 1: array_2bbp[i] = palette_color1; break;
+			case 2: array_2bbp[i] = palette_color2; break;
+			case 3: array_2bbp[i] = palette_color3; break;
+		}
+	}
 }
