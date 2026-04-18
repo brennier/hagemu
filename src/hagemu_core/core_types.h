159a8791f1d46d93d5c24fb3beeb830a50d2e1be
Bennett Rennier <bennett@brennier.com>
2026-04-18 19:26:21 +0900

    Make a core_types.h file containing all the shared types

---
+++ src/hagemu_core/core_types.h
@@ -0,0 +1,16 @@
+#ifndef HAGEMU_CORE_TYPES_H
+#define HAGEMU_CORE_TYPES_H
+
+// This is in the order that the buttons are defined in the hardware
+typedef enum HagemuButton {
+	HAGEMU_BUTTON_RIGHT,
+	HAGEMU_BUTTON_LEFT,
+	HAGEMU_BUTTON_UP,
+	HAGEMU_BUTTON_DOWN,
+	HAGEMU_BUTTON_A,
+	HAGEMU_BUTTON_B,
+	HAGEMU_BUTTON_SELECT,
+	HAGEMU_BUTTON_START,
+} HagemuButton;
+
+#endif
