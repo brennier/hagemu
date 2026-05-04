22069c00b2e078b601675a98842986dee4277f56
Bennett Rennier <bennett@brennier.com>
2026-05-04 10:52:02 +0900

    Clean up the timer.c code

--- src/hagemu_core/timer.c
+++ src/hagemu_core/timer.c
@@ -1,4 +1,6 @@
 #include "timer.h"
+#include <stdio.h>
+#include <stdlib.h>
 #include "mmu.h"
 
 #define TIMER_DIVIDER 0xFF04
@@ -8,37 +10,38 @@
 
 struct HagemuTimer {
 	uint16_t time; // measured in t-cycles
-	uint8_t control;
-	uint8_t divider;
-	uint8_t modulo;
-	uint8_t counter;
-	bool is_running;
+	uint8_t  divider;
+	uint8_t  modulo;
+	uint8_t  counter;
+	uint8_t  clock_select;
+	bool     is_running;
+	bool     enabled;
 } timer = { .is_running = true };
 
 uint8_t timer_register_read(uint16_t address) {
 	switch (address) {
-	case TIMER_DIVIDER:
-		// get the time from timer.h
-		return ((timer.time & 0xFF00) >> 8);
+	case TIMER_DIVIDER: return timer.time >> 8;
 	case TIMER_COUNTER: return timer.counter;
-	case TIMER_MODULO: return timer.modulo;
-	case TIMER_CONTROL:
-		// bits 3 through 7 should always be 1
-		return timer.control | 0xF8;
+	case TIMER_MODULO:  return timer.modulo;
+	case TIMER_CONTROL: return 0xF8 | (timer.enabled << 2) | timer.clock_select;
+	default:
+		fprintf(stderr, "[ERROR] Read from illegal timer address %04X\n", address);
+		exit(EXIT_FAILURE);
 	}
 }
 
 void timer_register_write(uint16_t address, uint8_t value) {
 	switch(address) {
-	case TIMER_DIVIDER:
-		timer.time = 0;
-		return;
+	case TIMER_DIVIDER: timer.time = 0;        return;
 	case TIMER_COUNTER: timer.counter = value; return;
-	case TIMER_MODULO:  timer.modulo = value; return;
+	case TIMER_MODULO:  timer.modulo = value;  return;
 	case TIMER_CONTROL:
-		value &= 0x07; // Mask all but the lowest 3 bits
-		timer.control = value;
+		timer.enabled      = value & (1 << 2);
+		timer.clock_select = value & 0x03;
 		return;
+	default:
+		fprintf(stderr, "[ERROR] Write to illegal timer address %04X\n", address);
+		exit(EXIT_FAILURE);
 	}
 }
 
@@ -55,15 +58,18 @@ void timer_tick() {
 		timer.time += 4;
 
 	// Return early if timer control is off
-	if (!(timer.control & (1 << 2)))
+	if (!timer.enabled)
 		return;
 
 	unsigned increment_factor;
-	switch (timer.control & 0x3) {
+	switch (timer.clock_select) {
         case 0x00: increment_factor = 1024; break;
         case 0x01: increment_factor = 16;   break;
         case 0x02: increment_factor = 64;   break;
         case 0x03: increment_factor = 256;  break;
+	default:
+		fprintf(stderr, "[ERROR] Illegal timer clock_select value %02X\n", timer.clock_select);
+		exit(EXIT_FAILURE);
 	}
 
 	if (timer.time % increment_factor != 0)
