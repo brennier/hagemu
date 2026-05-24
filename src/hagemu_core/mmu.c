0469ab732344deb469b4d5dceae46783018bbe50
Bennett Rennier <bennett@brennier.com>
2026-05-24 19:26:21 +0900

    Add the additional wram banks for CGB mode

--- src/hagemu_core/mmu.c
+++ src/hagemu_core/mmu.c
@@ -13,12 +13,13 @@
 #include "boot.h"
 #include "interrupt.h"
 
-#define WORK_RAM_SIZE 0x2000 // 8 kilobytes
-#define HIGH_RAM_SIZE 0x80   // 128 bytes
+#define WRAM_BANK_SIZE 0x1000 // 4 kilobytes
+#define HIGH_RAM_SIZE  0x80   // 128 bytes
 
 struct HagemuMMU {
 	bool boot_rom_ignore;
-	uint8_t wram[WORK_RAM_SIZE];
+	unsigned wram_bank;
+	uint8_t wram[8][WRAM_BANK_SIZE];
 	uint8_t hram[HIGH_RAM_SIZE];
 	uint8_t serial_data;
 	uint8_t serial_control;
@@ -26,6 +27,7 @@ struct HagemuMMU {
 
 void mmu_reset(void) {
 	memset(&mmu, 0, sizeof(struct HagemuMMU));
+	mmu.wram_bank = 1;
 }
 
 static uint8_t mmu_read_io(uint16_t address) {
@@ -36,6 +38,9 @@ static uint8_t mmu_read_io(uint16_t address) {
 	case 0xFF0F: return interrupt_register_read();
 	case 0xFF46: return dma_read();
 	case 0xFF50: return mmu.boot_rom_ignore;
+#ifdef CGB_MODE
+	case 0xFF70: return mmu.wram_bank | 0xF8;
+#endif
 	}
 
 	if (address >= 0xFF04 && address <= 0xFF07)
@@ -56,6 +61,9 @@ static void mmu_write_io(uint16_t address, uint8_t value) {
 	case 0xFF0F: interrupt_register_write(value); return;
 	case 0xFF46: dma_start(value);                return;
 	case 0xFF50: mmu.boot_rom_ignore = true;      return;
+#ifdef CGB_MODE
+	case 0xFF70: mmu.wram_bank = value & 0x07;    return;
+#endif
 	}
 
 	if (address >= 0xFF04 && address <= 0xFF07)
@@ -86,18 +94,22 @@ uint8_t mmu_read_nonblocking(uint16_t address) {
 	case 0xA000: case 0xB000:
 		return cart_ram_read(address - 0xA000);
 
-	// Work RAM (8 KiB)
-	case 0xC000: case 0xD000:
-		return mmu.wram[address - 0xC000];
+	// Work RAM (Bank 0) (4 KiB)
+	case 0xC000:
+		return mmu.wram[0][address - 0xC000];
+
+	// Work RAM (Swappable bank) (4 KiB)
+	case 0xD000:
+		return mmu.wram[mmu.wram_bank][address - 0xD000];
 
 	// Top half of echo RAM (4 KiB)
 	case 0xE000:
-		return mmu.wram[address - 0xE000];
+		return mmu.wram[0][address - 0xE000];
 
 	case 0xF000:
 		// Bottom half of echo RAM (about 4 KiB)
 		if (address < 0xFE00)
-			return mmu.wram[address - 0xE000];
+			return mmu.wram[mmu.wram_bank][address - 0xF000];
 		// Object Attribute Memory
 		else if (address < 0xFEA0)
 			return ppu_oam_read(address - 0xFE00);
@@ -138,20 +150,25 @@ void mmu_write_nonblocking(uint16_t address, uint8_t value) {
 		cart_ram_write(address - 0xA000, value);
 		return;
 
-	// Work RAM (8 KiB)
-	case 0xC000: case 0xD000:
-		mmu.wram[address - 0xC000] = value;
+	// Work RAM (Bank 0) (4 KiB)
+	case 0xC000:
+		mmu.wram[0][address - 0xC000] = value;
+		return;
+
+	// Work RAM (Swappable bank) (4 KiB)
+	case 0xD000:
+		mmu.wram[mmu.wram_bank][address - 0xD000] = value;
 		return;
 
 	// Top half of echo RAM (4 KiB)
 	case 0xE000:
-		mmu.wram[address - 0xE000] = value;
+		mmu.wram[0][address - 0xE000] = value;
 		return;
 
 	case 0xF000:
 		// Bottom half of echo RAM (about 4 KiB)
 		if (address < 0xFE00)
-			mmu.wram[address - 0xE000] = value;
+			mmu.wram[mmu.wram_bank][address - 0xF000] = value;
 		// Object Attribute Memory
 		else if (address < 0xFEA0)
 			ppu_oam_write(address - 0xFE00, value);
