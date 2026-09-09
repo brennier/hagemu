7423383a4dfbbf9a390f8b10490e754375f9e5cd
Bennett Rennier <barennier@gmail.com>
2026-09-09 17:26:03 +0900

    Reset APU channel ticks during trigger. Now passes most of Blargg's cgb_sound test #12.

--- src/hagemu_core/apu.c
+++ src/hagemu_core/apu.c
@@ -503,6 +503,7 @@ static void channel_trigger(struct Channel *ch, int length_max) {
 			ch->length_current--;
 		}
 	}
+        ch->ticks = 0;
 	ch->envelope_current = 0;
 	ch->volume_current = ch->volume_initial;
 	ch->duty_wave_index = 0;
@@ -728,8 +729,11 @@ void apu_register_write(uint16_t address, uint8_t value) {
 	case 0xFF34: case 0xFF35: case 0xFF36: case 0xFF37:
 	case 0xFF38: case 0xFF39: case 0xFF3A: case 0xFF3B:
 	case 0xFF3C: case 0xFF3D: case 0xFF3E: case 0xFF3F:
-		if (!apu.ch3.enabled)
+		if (apu.ch3.enabled) {
+			apu.wave_data[apu.ch3.wave_index / 2] = value;
+		} else {
 			apu.wave_data[address - APU_WAVE_DATA_START] = value;
+		}
 		return;
 
 	default:
@@ -788,9 +792,8 @@ uint8_t apu_register_read(uint16_t address) {
 	case 0xFF38: case 0xFF39: case 0xFF3A: case 0xFF3B:
 	case 0xFF3C: case 0xFF3D: case 0xFF3E: case 0xFF3F:
 		if (apu.ch3.enabled) {
-			return apu.ch3.wave_sample_buffer;
-		}
-		else {
+			return apu.wave_data[apu.ch3.wave_index / 2];
+		} else {
 			return apu.wave_data[address - APU_WAVE_DATA_START];
 		}
 
